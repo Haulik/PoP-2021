31e83a033aa42120b79adb586aed71a1b8529692
Haulik <thomasbarchager@hotmail.dk>
2021-11-02 11:33:02 +0100

    Start of the Assignment

--- Assignment/7g/src/rotate.fs
+++
@@ -1,5 +0,0 @@
-module Rotate
-
-let listabc = ['a'; 'b'; 'c'; 'd'; 'e'; 'f'; 'g'; 'h'; 'i'; 'j'; 'k'; 'l'; 'n'; 'm'; 'o'; 'p'; 'q'; 'r'; 's'; 't'; 'u'; 'v'; 'w'; 'x'; 'y'; 'z']
-
-let list1to5 = [ 1 .. n ]
---
+++ Assignment/7g/src/rotate.fsx
@@ -0,0 +1,24 @@
+//module Rotate
+
+type Board = char list
+
+let listabc = ['a'..'z']
+
+let create (n:uint) : Board =
+    listabc.[0 .. (int(n)*int(n))-1]
+    
+
+let rec board2Str (b:Board) : string =
+    let length = int(sqrt(float b.Length)) 
+    printfn "%d" length
+    match b with
+        |[] -> ""
+        |[x] -> x.ToString()
+        | x :: ys -> x.ToString() + board2Str ys
+
+
+
+
+
+
+printfn "%A" (board2Str (create 3u))
