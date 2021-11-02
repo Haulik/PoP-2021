68ee4f3f33d51ae0376913fbae00106a9b06d2a0
Haulik <thomasbarchager@hotmail.dk>
2021-11-02 10:48:13 +0100

    abc

---
+++ Assignment/7g/src/rotate.fs
@@ -0,0 +1,5 @@
+module Rotate
+
+let listabc = ['a'; 'b'; 'c'; 'd'; 'e'; 'f'; 'g'; 'h'; 'i'; 'j'; 'k'; 'l'; 'n'; 'm'; 'o'; 'p'; 'q'; 'r'; 's'; 't'; 'u'; 'v'; 'w'; 'x'; 'y'; 'z']
+
+let list1to5 = [ 1 .. n ]
