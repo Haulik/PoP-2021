e8486034c873c72959155d162817e8ccacb2e52d
sebbex1337 <sefort@hotmail.dk>
2021-11-11 14:18:26 +0100

    Whiteboxtests

--- Assignment/7g/src/whiteboxtest.fsx
+++ Assignment/7g/src/whiteboxtest.fsx
@@ -15,6 +15,7 @@ printfn "uint : 5u      branch : 1c      Expected: ['a'..'y']"
 printfn "Result %A" (Rotate.create 5u)
 printfn "passed: %b" (Rotate.create 5u = ['a';'b';'c';'d';'e';'f';'g';'h';'i';'j';'k';'l';'m';'n';'o';'p';'q';'r';'s';'t';'u';'v';'w';'x';'y'])
 printfn ""
+
 printfn "Whitebox test of board2Str (b:Board) : string"
 printfn "b: []      branch : 1-1a        Expected: \"\""
 printfn "Result %A" (Rotate.board2Str [])
@@ -32,6 +33,7 @@ printfn "b: ['a'..'y']      branch : 1-2c/d        Expected: \"\nabcde\nfghij\nk
 printfn "Result %A" (Rotate.board2Str ['a'..'y'])
 printfn "passed: %b" (Rotate.board2Str ['a'..'y'] = "\nabcde\nfghij\nklmno\npqrst\nuvwxy\n\n")
 printfn ""
+
 printfn "Whitebox test of validRotate (b:Board) (p:Position) : bool"
 printfn "b: ['a'..'i']      p: -1       branch : 1a       Expected: false"
 printfn "Result %b" (Rotate.validRotate (Rotate.create 3u) -1)
@@ -49,7 +51,20 @@ printfn "b: ['a'..'i']      p: -1       branch : 1a       Expected: false"
 printfn "Result %b" (Rotate.validRotate (Rotate.create 3u) -1)
 printfn "passed: %b" (Rotate.validRotate (Rotate.create 3u) -1 = false)
 printfn ""
+
 printfn "Whitebox test of rotate (b:Board) (p:Position) : Board"
 printfn "b: ['a'..'i']      p: -1        branch : 1a      Expected: ['a';'b';'c';'d';'e';'f';'g';'h';'i']"
 printfn "Result %A" (Rotate.rotate (Rotate.create 3u) -1)
 printfn "passed: %b" (Rotate.rotate (Rotate.create 3u) -1 = ['a'..'i'])
+
+printfn "Whitebox test of scramble (b:Board) (m:uint) : Board"
+
+printfn "Whitebox test of solved (b:Board) : bool"
+printfn "b: ['a'..'i']      branch : 1a         Expected: true"
+printfn "Result %A" (Rotate.solved (Rotate.create 3u))
+printfn "passed %b" (Rotate.solved (Rotate.create 3u) = true)
+printfn ""
+printfn "b: ['a';'c';'b';'d';'f';'g';'e';'h';'i']      branch : 1b         Expected: false"
+printfn "Result %A" (Rotate.solved (Rotate.create 3u))
+printfn "passed %b" (Rotate.solved (['a';'c';'b';'d';'f';'g';'e';'h';'i']) = false)
+printfn ""
